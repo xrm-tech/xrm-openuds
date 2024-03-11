ec8942e4c0a5c498355a3eec3bc3182e4822c424
Nikolai Spitsa <n.spitsa@pvhostvm.ru>
2024-03-11 18:10:24 +0500

    фикс полей шаблона под скрипт Андрея

--- actions/run_generate.py
+++ actions/run_generate.py
@@ -32,18 +32,18 @@ class RunGenerate(Action):
     
     def run(self, plan_name):
 
-        data = {"broker_primary_url": self.config['01_broker_primary_url'],\
+        data = {"broker_primary_ip": self.config['01_broker_primary_ip'],\
                 "broker_primary_username": self.config['02_broker_primary_username'],\
                 "broker_primary_auth":self.config['03_broker_primary_authenticator'],\
                 "broker_primary_password": self.config['04_broker_primary_password'],\
-                "broker_secondary_url": self.config['05_broker_secondary_url'],\
+                "broker_secondary_ip": self.config['05_broker_secondary_ip'],\
                 "broker_secondary_username": self.config['06_broker_secondary_username'], \
                 "broker_secondary_auth": self.config['07_broker_secondary_authenticator'],\
                 "broker_secondary_password": self.config['08_broker_secondary_password'], \
                 "service_pool_name": self.config['09_service_pool_name']}
 
         self.get_service_pool_info(
-            broker_ip= data['broker_primary_url'],
+            broker_ip= data['broker_primary_ip'],
             broker_user= data['broker_primary_username'],
             broker_auth= data['broker_primary_auth'],
             broker_pwd= data['broker_primary_password'],
