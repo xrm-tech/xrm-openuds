644e922d2707312b1b0be4516c32b5b1025d526b
Nikolai Spitsa <n.spitsa@pvhostvm.ru>
2024-03-29 02:38:41 +0500

    test сохранения данных в отдельную директорию

--- actions/run_generate.py
+++ actions/run_generate.py
@@ -22,7 +22,7 @@ class RunGenerate(Action):
         plan_data.append({'transport': transport_param})
         plan_data.append({'permissions': permissions_param})
 
-        packs_path= '/opt/stackstorm/saved/'
+        packs_path= '/opt/stackstorm/packs/'
         plan_ending= '.plandata'
         plan_full_name= os.path.join(packs_path, plan, plan_ending)
 
