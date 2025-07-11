ac09275c43c8eb2fc0fbd5f990f4196ca16df5a5
Nikolai Spitsa <n.spitsa@pvhostvm.ru>
2025-07-11 12:30:59 +0500

    Убран избыточный файл

--- vdi/ds.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem -Recurse -File -Include *.py | 
-Where-Object { 
-    $_.FullName -notmatch '\\\.git|\\build|\\dart_tool|\\android\\app\\build|\\ios' 
-} | 
-ForEach-Object { 
-    "`n`n=== Τΰιλ: $($_.FullName) ===`n" 
-    Get-Content $_.FullName 
-} | 
-Out-File combined.txt
